f67b32203d822ab42263a35f605404b3ccbb00ca
Jesús Soto <jesus.soto@pucp.edu.pe>
2023-09-08 12:49:51 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from dotenv import load_dotenv
+
+
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title='Chat with multiple PDFs', page_icon=':books:')
+    
+    
+    st.header("Chat with Multiple PDFs :books:")
+    st.text_input('Ask a question about your documents:')
+
+    with st.sidebar:
+        st.subheader('Your documents')
+        st.file_uploader("Upload your PDFs and clic on 'Process'")
+        st.button('Process')
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
