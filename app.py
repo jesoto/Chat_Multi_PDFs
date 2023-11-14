19ab5f8cad1f3657db13eea45de090986b8e4c30
Jesús Soto <jesus.soto@pucp.edu.pe>
2023-11-14 00:24:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
+from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -47,17 +48,38 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question':user_question})
+    st.session_state.chat_history = response['chat_history']
+    
+    for i , message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content),unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content),unsafe_allow_html=True)
+    
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title='Chat with multiple PDFs', page_icon=':books:')
+    st.set_page_config(page_title='Chat with multiple PDFs',
+                       page_icon=':books:')
+    
+    st.write(css, unsafe_allow_html=True)
     
     if 'conversation' not in st.session_state:
         st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
     
     st.header("Chat with Multiple PDFs :books:")
-    st.text_input('Ask a question about your documents:')
+    user_question = st.text_input('Ask a question about your documents:')
+    if user_question:
+        handle_userinput(user_question)
+        
 
+    
     with st.sidebar:
         st.subheader('Your documents')
         pdf_docs = st.file_uploader("Upload your PDFs and clic on 'Process'", accept_multiple_files=True)
